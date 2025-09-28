5d6204e276790b6aca91e93c22cd3ec82b0eee96
kamalkavin96 <kamalkavin68@gmail.com>
2025-09-28 13:29:42 +0530

    Refactor query handling in job search service and update URL query to dictionary conversion

--- src/utils/url_query_to_dict_convertor.py
+++ src/utils/url_query_to_dict_convertor.py
@@ -1,5 +1,5 @@
 from urllib.parse import parse_qs, unquote
 
 def url_query_to_dict(query: str):
-    query_dict = {k: v[0] if len(v) == 1 else v for k, v in parse_qs(query).items()}
+    query_dict = {k: unquote(v[0]) for k, v in parse_qs(query).items()}
     return query_dict
