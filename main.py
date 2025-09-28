a1b461206ea281d5a63231ba815ca489e026adbe
kamalkavin96 <kamalkavin68@gmail.com>
2025-09-28 13:31:15 +0530

    Refactor main.py to streamline query processing and enhance job data retrieval

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from src.utils.url_query_to_dict_convertor import url_query_to_dict
 
 
 query = """noOfResults=100&urlType=search_by_keyword&searchType=adv&keyword=developer%2C%20software%20developer%2C%20software%20engineer%2C%20software%20development&sort=f&pageNo=1&jobAge=1&jobPostType=1&k=developer%2C%20software%20developer%2C%20software%20engineer%2C%20software%20development&jobAge=1&jobPostType=1&seoKey=developer-software-developer-software-engineer-software-development-jobs&src=cluster&latLong=13.0351104_80.232448&sid=17590449057226043_6"""
-print(url_query_to_dict(query=query))
+query_data = url_query_to_dict(query=query)
+job_data = job_search_service_provider(query_params=query_data)
+
+print(job_data)
